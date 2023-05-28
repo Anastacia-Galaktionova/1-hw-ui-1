56673ebebd4d4d5e177c370b5ffe531b45c1a065
anastacia_gal <anastacia_gal@mail.ru>
2023-05-28 22:43:58 +0300

    3 пункт

--- src/main/java/com/company/homeworkloans/screen/loan/LoanBrowse.java
+++ src/main/java/com/company/homeworkloans/screen/loan/LoanBrowse.java
@@ -1,10 +1,16 @@
 package com.company.homeworkloans.screen.loan;
 
-import io.jmix.ui.component.Table;
+import com.company.homeworkloans.entity.LoanStatus;
+import io.jmix.core.DataManager;
+import io.jmix.ui.Notifications;
+import io.jmix.ui.action.Action;
+import io.jmix.ui.component.GroupTable;
 import io.jmix.ui.model.CollectionLoader;
 import io.jmix.ui.screen.*;
 import com.company.homeworkloans.entity.Loan;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
 
 @UiController("Loan.browse")
 @UiDescriptor("loan-browse.xml")
@@ -13,6 +19,12 @@ public class LoanBrowse extends StandardLookup<Loan> {
 
     @Autowired
     private CollectionLoader<Loan> loansDl;
+    @Autowired
+    private GroupTable<Loan> loansTable;
+    @Autowired
+    private DataManager dataManager;
+    @Autowired
+    private Notifications notifications;
 
 
    /* @Subscribe
@@ -25,6 +37,31 @@ public class LoanBrowse extends StandardLookup<Loan> {
         loansDl.setParameter("statusId", "R");
         loansDl.load();
     }
+
+    @Subscribe("approveAction")
+    public void onApproveAction(Action.ActionPerformedEvent event) {
+
+        Set<Loan> loanToApprove = loansTable.getSelected();
+        Loan loan = loanToApprove.stream().findFirst().get();
+        loan.setStatus(LoanStatus.APPROVED);
+        dataManager.save(loan);
+        loansDl.load();
+        notifications.create().withCaption("Approved").show();
+
+    }
+
+    @Subscribe("rejectAction")
+    public void onRejectAction(Action.ActionPerformedEvent event) {
+        Set<Loan> loanToApprove = loansTable.getSelected();
+        Loan loan = loanToApprove.stream().findFirst().get();
+        loan.setStatus(LoanStatus.REJECTED);
+        dataManager.save(loan);
+        loansDl.load();
+        notifications.create().withCaption("Rejected").show();
+    }
+
+
+
 
     /*@Subscribe
     public void onInit(InitEvent event) {
