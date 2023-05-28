a368e10fb4a3eccd532e97e469cf8ce2f11142af
anastacia_gal <anastacia_gal@mail.ru>
2023-05-28 19:00:58 +0300

    1, 2 пункты

--- src/main/java/com/company/homeworkloans/screen/client/ClientBrowse.java
+++ src/main/java/com/company/homeworkloans/screen/client/ClientBrowse.java
@@ -1,10 +1,34 @@
 package com.company.homeworkloans.screen.client;
 
+import com.company.homeworkloans.screen.requestloan.Requestloan;
+import io.jmix.ui.ScreenBuilders;
+import io.jmix.ui.component.GroupTable;
 import io.jmix.ui.screen.*;
 import com.company.homeworkloans.entity.Client;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("Client.browse")
 @UiDescriptor("client-browse.xml")
 @LookupComponent("clientsTable")
 public class ClientBrowse extends StandardLookup<Client> {
+    @Autowired
+    private ScreenBuilders screenBuilders;
+    @Autowired
+    private GroupTable<Client> clientsTable;
+
+    @Subscribe("requestLoanAction")
+    public void onRequestLoanAction(io.jmix.ui.action.Action.ActionPerformedEvent event) {
+        Client selected = clientsTable.getSingleSelected();
+        if (selected == null) {
+            return;
+        }
+
+        screenBuilders.screen(this)
+                .withScreenClass(Requestloan.class)
+                .withOpenMode(OpenMode.DIALOG)
+                .build()
+                .withClient(selected)
+                .show();
+
+    }
 }
--- src/main/java/com/company/homeworkloans/screen/loan/LoanBrowse.java
+++ src/main/java/com/company/homeworkloans/screen/loan/LoanBrowse.java
@@ -1,10 +1,40 @@
 package com.company.homeworkloans.screen.loan;
 
+import io.jmix.ui.component.Table;
+import io.jmix.ui.model.CollectionLoader;
 import io.jmix.ui.screen.*;
 import com.company.homeworkloans.entity.Loan;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("Loan.browse")
 @UiDescriptor("loan-browse.xml")
 @LookupComponent("loansTable")
 public class LoanBrowse extends StandardLookup<Loan> {
+
+    @Autowired
+    private CollectionLoader<Loan> loansDl;
+
+
+   /* @Subscribe
+    public void onInit(InitEvent event) {
+        loansDl.setParameter("statusId", "R");
+    }*/
+
+    @Subscribe
+    public void onBeforeShow(BeforeShowEvent event) {
+        loansDl.setParameter("statusId", "R");
+        loansDl.load();
+    }
+
+    /*@Subscribe
+    public void onInit(InitEvent event) {
+        loansTable.addGeneratedColumn("Age", entity -> {
+
+        });
+    }*/
+
+
+
+
+
 }
